67135ee110e8c252d9fbb73a2bdd0ba1352360f5
emanwael39 <101505706+emanwael39@users.noreply.github.com>
2024-11-28 07:34:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
 
+import "./App.css";
+import MainComponent from "./components/MainComponent";
+import "./App.css";
+import { Container } from "@mui/material";
+// import PrayerTimesByCity from "./components/test";
+
+function App() {
+  return (
+    <>
+      <div style={{ width: "100vw" }}>
+        <Container>
+          <MainComponent />
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default App;
